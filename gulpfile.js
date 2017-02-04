da6231c49807a71a007086b0ef71bab8c8552628
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2017-02-03 22:30:32 -0800

    out with the Grunt, in with the Gulp

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const babel = require('gulp-babel')
+const sourcemaps = require('gulp-sourcemaps')
+const connect = require('gulp-connect')
+
+// HTML
+gulp.task('html', () => {
+  return gulp.src('./src/index.html')
+    .pipe(gulp.dest('./build'))
+    .pipe(connect.reload())
+})
+
+// Styles
+gulp.task('styles', () => {
+  const postCssPlugins = [
+    autoprefixer()
+  ]
+
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(postCssPlugins))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(connect.reload())
+})
+
+// JS
+gulp.task('babel', () => {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/js'))
+    .pipe(connect.reload())
+})
+
+// Connect
+gulp.task('connect', () => {
+  connect.server({
+    root: './build',
+    livereload: true
+  })
+})
+
+// Watch
+gulp.task('watch', () => {
+  gulp.watch('./src/*.html', ['html'])
+  gulp.watch('./src/scss/**/*.scss', ['styles'])
+  gulp.watch('./src/js/**/*.js', ['babel'])
+})
+
+gulp.task('default', ['html', 'styles', 'babel', 'connect', 'watch'])
